224e737fe0baac8be946b6fa9b0a03022e87d9f1
Nikita Ivanov <nikita.ivanov@truesalessoft.com>
2022-10-04 20:43:40 +0300

    added  - login/logout on page

--- SecurityApp/Pages/Account/Login.cshtml.cs
+++ SecurityApp/Pages/Account/Login.cshtml.cs
@@ -37,7 +37,12 @@ namespace SecurityApp.Pages.Account
 
                 ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
 
-                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+                var authProperties = new AuthenticationProperties
+                {
+                    IsPersistent = Credential.RememberMe
+                };
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal, authProperties);
 
                 return RedirectToPage("/Index");
             }                          
@@ -56,5 +61,8 @@ namespace SecurityApp.Pages.Account
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
     }
 }
--- SecurityApp/Program.cs
+++ SecurityApp/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddAuthentication("MyCookieAuth").AddCookie("MyCookieAuth", opt
     options.Cookie.Name = "MyCookieAuth";
     options.LoginPath = "/Account/Login";
     options.AccessDeniedPath = "/Account/AccessDenied";
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(2);
 });
 
 builder.Services.AddAuthorization(options =>
